bc0bc0f36a5f6e1c5ca7b00e54d4cea14eda0a5e
Kris Wong <krischwong@gmail.com>
2024-11-27 09:14:49 -0500

    Added basic route and view for index

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("node:path");
+
+const indexRouter = require("./routes/indexRouter");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
+});
