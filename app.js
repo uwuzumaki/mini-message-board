89746e03b724034eb0cfbc83056f7f4a7728d13f
Kris Wong <krischwong@gmail.com>
2024-11-27 20:08:11 -0500

    Added rendering of pre-made message

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("node:path");
 
 const indexRouter = require("./routes/indexRouter");
+const newRouter = require("./routes/newRouter");
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use("/", indexRouter);
+app.use("/new", newRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
---
+++ routes/newRouter.js
@@ -0,0 +1,9 @@
+const Router = require("express");
+
+const newRouter = Router();
+
+newRouter.get("/", (req, res) => {
+  res.render("new");
+});
+
+module.exports = newRouter;
